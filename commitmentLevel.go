cdbf75a1f364b8ec9968baab1f7c55f33d2a92c8
Bernard Bussy <brbitzbussy@gmail.com>
2021-10-03 14:14:36 +0200

    add comments to commiitment level

--- commitmentLevel.go
+++ commitmentLevel.go
@@ -1,14 +1,24 @@
 package solana
 
+// CommitmentLevel is the level of commitment desired when querying state
 type CommitmentLevel string
 
 var (
-	ProcessedCommitmentLevel    CommitmentLevel = "processed"
-	ConfirmedCommitmentLevel    CommitmentLevel = "confirmed"
-	FinalizedCommitmentLevel    CommitmentLevel = "finalized"
-	RecentCommitmentLevel       CommitmentLevel = "recent"
-	SingleCommitmentLevel       CommitmentLevel = "single"
-	SingleGossipCommitmentLevel CommitmentLevel = "singleGossip"
-	RootCommitmentLevel         CommitmentLevel = "root"
-	MaxCommitmentLevel          CommitmentLevel = "max"
+	// ProcessedCommitmentLevel indicates to query the most recent
+	// block which has reached 1 confirmation by the connected node
+	ProcessedCommitmentLevel CommitmentLevel = "processed"
+
+	// ConfirmedCommitmentLevel indicates to query the most recent
+	// block which has reached 1 confirmation by the cluster
+	ConfirmedCommitmentLevel CommitmentLevel = "confirmed"
+
+	// FinalizedCommitmentLevel indicates to query the most recent
+	// block which has been finalized by the cluster
+	FinalizedCommitmentLevel CommitmentLevel = "finalized"
+
+	RecentCommitmentLevel       CommitmentLevel = "recent"       // Deprecated as of v1.5.5
+	SingleCommitmentLevel       CommitmentLevel = "single"       // Deprecated as of v1.5.5
+	SingleGossipCommitmentLevel CommitmentLevel = "singleGossip" // Deprecated as of v1.5.5
+	RootCommitmentLevel         CommitmentLevel = "root"         // Deprecated as of v1.5.5
+	MaxCommitmentLevel          CommitmentLevel = "max"          // Deprecated as of v1.5.5
 )
