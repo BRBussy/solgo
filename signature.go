71a2921b3c364b8433595766a08d6baf3405e9a0
Bernard Bussy <brbitzbussy@gmail.com>
2021-10-16 07:54:42 +0200

    prep testing

--- signature.go
+++ signature.go
@@ -7,6 +7,11 @@ import (
 // Signature is a digital signature in the ed25519 binary format and consumes 64 bytes
 type Signature [64]byte
 
+// Bytes returns the Signature as a byte slice instead of fixed size array.
+func (s Signature) Bytes() []byte {
+	return s[:]
+}
+
 // Signatures is a list of Signature entries.
 // It implements the encoding.Compactor interface so that
 // it can be converted into an encoding.CompactArray of signatures.
@@ -14,8 +19,17 @@ type Signatures []Signature
 
 // Compact Signatures into an encoding.CompactArray
 func (s Signatures) Compact() (encoding.CompactArray, error) {
+	// prepare slice of data to return
+	data := make([]byte, len(s))
+
+	// pack all signatures into the data slice
+	for i := range s {
+		data = append(data, s[i].Bytes()...)
+	}
+
+	// and return
 	return encoding.CompactArray{
-		Length: 0,
-		Data:   nil,
+		Length: uint64(len(s)),
+		Data:   data,
 	}, nil
 }
